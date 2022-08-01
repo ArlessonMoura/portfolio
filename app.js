de6ca9c7e4d7a4055a10d5614432ffd50c360e7b
ArlessonMoura <arlessonmss@gmail.com>
2022-08-01 18:38:49 -0300

    touch bug

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // HEROKU BUG
-
 let dark = false;
 
 const darkMode = () => {
@@ -16,6 +15,34 @@ const darkMode = () => {
     dark = false;
     return dark;
   }
+}
+
+function watchForHover() {
+  // lastTouchTime is used for ignoring emulated mousemove events
+  // that are fired after touchstart events. Since they're
+  // indistinguishable from real events, we use the fact that they're
+  // fired a few milliseconds after touchstart to filter them.
+  let lastTouchTime = 0;
+
+  function enableHover() {
+    if (new Date() - lastTouchTime < 500) {
+      return document.body.classList.add('hasHover');
+    }
+  }
+
+  function disableHover() {
+    document.body.classList.remove('hasHover');
+  }
+
+  function updateLastTouchTime() {
+    lastTouchTime = new Date();
+  }
+
+  document.addEventListener('touchstart', updateLastTouchTime, true);
+  document.addEventListener('touchstart', disableHover, true);
+  document.addEventListener('mousemove', enableHover, true);
+
+  enableHover();
 }
 
 const dynamicMenu = () => {
@@ -109,3 +136,4 @@ const dynamicSettings = () => {
 }
           
 dynamicSettings();
+watchForHover();
